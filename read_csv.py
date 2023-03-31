c2f2d3270d049aa439a7b1aee62f600f878e5f21
gabriellayuwono <125839000+gabriellayuwono@users.noreply.github.com>
2023-03-31 10:47:19 +0800

    Add files via upload

--- read_csv.py
+++ read_csv.py
@@ -6,10 +6,11 @@ import pandas as pd
 # 1. Input
 raw_data = pd.read_csv("Menu.csv")
 print(raw_data)
-print(raw_data.info())
 
 # 2. Process
-total = raw_data["Price"].sum()
+Numberofitems = len(raw_data)
+Sorted_data = raw_data.sort_values ("Menu",ascending=True)
 
 # 3. Output
-print(f"Total: {total}")
+print(f"Count:{Numberofitems}")
+print(f'{Sorted_data}')
